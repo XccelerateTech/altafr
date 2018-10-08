87a1562827fc8b50b65e30ee2b9ea4a4b96b08b3
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2018-10-08 17:54:15 +0800

    minor answer updates

--- web/d7/C.js
+++ web/d7/C.js
@@ -45,27 +45,34 @@ while(gambler1.cash > 0 || gambler2.cash > 0){
 
 console.log('The order of players running out of cash:', outOfCashOrder);
 
+
+
+
+
+
 //method two
 
-class Gambler {
-    constructor(options){
+class Gambler { //create a class called Gambler - use to initialize the gamblers
+    constructor(options){ //the properties needed when creating the gambler objects
       this.cash = options.cash;
       this.bet = options.bet;
       this.lossProbability = options.lossProbability;
     }
-    lose(){
+    lose(){ //function every gambler has, to indicate how much money the gambler uses --> points to the bet of the initialized gambler - instances cash - bet.
       this.cash -= Math.min(this.cash, this.bet)
     }
   }
   
-  var gambler1 = new Gambler({cash:100,bet:15,lossProbability:0.6});
+  var gambler1 = new Gambler({cash:100,bet:15,lossProbability:0.6}); //create new gamblers -- who will have cash, a bet amount and a loss probability
   var gambler2 = new Gambler({cash:120,bet:10,lossProbability:0.4});  
-    var round = 0;
-    var outOfCashOrder = [];
+
+  //keep track of the number of rounds played, here we start a counter called round.
+  var round = 0;
+    var outOfCashOrder = []; //create an empty array to store the fools who lost all their cash
   
-  let play = function (name, currentGambler) {
+  let play = function (name, currentGambler) { //we have set up the players, defined some features of the game, here is the logic to see if someone wins or loses.
     if(currentGambler.cash > 0){
-          if(Math.random() < currentGambler.lossProbability){
+          if(Math.random() < currentGambler.lossProbability){ 
               currentGambler.lose();
               console.log('Gambler '+name+' lost, $'+currentGambler.cash+' remaining');
               if(currentGambler.cash<=0){
@@ -75,18 +82,20 @@ class Gambler {
           }
       }
   }
+
+  //using Math.random(), to check if the current gambler has lost money or not. 
   
-  let casino =function () {
-    play('Jim', gambler1);
+  let casino =function () { //here is the stage of the casino
+    play('Jim', gambler1); //call play() method with the parameters needed.
     play('Bill', gambler2)
   }
   
   console.log('Casino bets are in!');
-  while(gambler1.cash > 0 || gambler2.cash > 0){
+  while(gambler1.cash > 0 || gambler2.cash > 0){ //logic to keep the game going until they have lost all thier money
       round ++;
   
       console.log('---- Round '+round+ ' ----')
-      casino();
+      casino(); //invoke the function, let them play
   }
   
   console.log('The order of players running out of cash:', outOfCashOrder);
---
+++ web/d8/server/js/async.js
@@ -0,0 +1,14 @@
+
+//create a new XHLHttpRequest
+let http = new XMLHttpRequest();
+            http.open('GET','../data/data.json'); //target the file you want to GET
+            http.onreadystatechange = function(){//handle error and success
+                if(http.readyState != XMLHttpRequest.DONE) {
+                    return;
+                } else if(http.status == 200) {
+                    console.log(JSON.parse(http.responseText)); //get data into console 
+                } else {
+                    console.log('error occurred' + http.status); //tell user there is an error 
+                }
+            }
+            http.send(); //send the request
