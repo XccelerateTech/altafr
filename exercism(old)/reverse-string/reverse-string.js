0e45012a292eb54f426f3b5708c09dd5413d72f7
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2018-10-10 14:57:48 +0800

    updated exercism tracks

---
+++ exercism(old)/reverse-string/reverse-string.js
@@ -0,0 +1,24 @@
+function  reverseString (input) {
+    if (typeof(input) === "string") {
+        let temp = [];
+        let output = [];
+        for(let i = 0; i <input.length; i++){
+            temp[i] = input[i]
+        }
+        for(let i = temp.length - 1 ; i >= 0 ; i--){
+            output.push(temp[i])
+        }
+        return output.join('')
+    }
+}
+function advanced(input) {
+    if(typeof input === "string"){
+        return input.split('').reverse().join('');
+    }else {
+        return "Invalid Input"
+    }
+} 
+ 
+module.exports = advanced;
+
+
