40c0ab0e26e1229abf3427a4618d6be4927ce294
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2018-11-02 10:56:55 +0800

    added better comments

--- node/d24/app.js
+++ node/d24/app.js
@@ -1,18 +1,11 @@
+//require the promised fs made in a separate module as a well as the pathModule
 const promisedFS = require('./promisedFs');
 const pathModule = require('path');
 
-function traverseFolders(path){
-    promisedFS.readdir(path)
-    .then((files)=>{
-        for(let file of files){
-            const filePath = pathModule.join(path, file);
-            outPutFolderContent(filePath);
-        }
-    }).catch((error)=>{
-        console.log(error);
-    });
-}
+//the function outPutFolderContent, takes the filePath and an input, then we call the promisedFS.stat with the filePath, then we use a conditional statement to check if the file is a directory or not, if it is a directory we output the result and call the previous function traverseFolders (so that we can find each file or directory inside the directory we have examined) -- this will in turn pass the information back to this function to see if each output of filePath is a directory or a file. 
 
+//if the filePath is a file, we just output the console.log
+//if there is an error we catch it and console.log the result.
 function outPutFolderContent(filePath){
     promisedFS.stat(filePath).then((stats)=>{
         if(stats.isDirectory()){
@@ -26,4 +19,21 @@ function outPutFolderContent(filePath){
     });
 }
 
-outPutFolderContent('./files/files/..')
+
+//the function traverseFolders takes the input of the path to the folder, we call the promisedFS.readdir function, which produces an array of files (stored in our directory) a for loop is used to iterate over each of the files, and then we call our other function with the exact path to each file.
+function traverseFolders(path){
+    promisedFS.readdir(path)
+    .then((files)=>{
+        for(let file of files){
+            const filePath = pathModule.join(path, file);
+            outPutFolderContent(filePath);
+        }
+    }).catch((error)=>{
+        console.log(error);
+    });
+}
+
+
+
+//call the function
+outPutFolderContent('./files')
--- node/d24/promisedFs.js
+++ node/d24/promisedFs.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
+
+//use module.exports to declare the promisedFunction names and export them from the file.
+//basically wrap the native fs.stat in a promise and resolve with the information (stats) and rejst with the err
 module.exports.stat = (path) => {
     return new Promise((resolve, reject) => {
         fs.stat(path, (err, stats) => {
@@ -12,6 +15,7 @@ module.exports.stat = (path) => {
     });
 }
 
+//here we wrap the native fs.readdir in a promise and resolve with the array of files in our directory
 module.exports.readdir = (path)=>{
     return new Promise((resolve, reject)=>{
         fs.readdir(path,(err, files)=>{
